0ccecac032ae0c77eb87a42efa8cf104eaace5dc
Keen <keencwilson@gmail.com>
2022-02-01 19:53:04 -0800

    Formatted with prettier

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,7 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { createSelector } from 'reselect';
+import { createSlice } from '@reduxjs/toolkit'
+import { createSelector } from 'reselect'
 
-let lastId = 0;
+let lastId = 0
 
 // Internally createSlice will call createAction() and createReducer()
 const slice = createSlice({
@@ -14,28 +14,28 @@ const slice = createSlice({
         id: ++lastId,
         description: action.payload.description,
         resolved: false
-      });
+      })
     },
     bugAssignedToUser: (bugs, action) => {
-      const { bugId, userId } = action.payload;
-      const index = bugs.findIndex(bug => bug.id === bugId);
-      bugs[index].userId = userId;
+      const { bugId, userId } = action.payload
+      const index = bugs.findIndex(bug => bug.id === bugId)
+      bugs[index].userId = userId
     },
     bugResolved: (bugs, action) => {
-      const index = bugs.findIndex(bug => bug.id === action.payload.id);
-      bugs[index].resolved = true;
+      const index = bugs.findIndex(bug => bug.id === action.payload.id)
+      bugs[index].resolved = true
     },
     bugRemoved: (bugs, action) => {
-      bugs.filter(bug => bug.id !== action.payload.id);
+      bugs.filter(bug => bug.id !== action.payload.id)
     }
   }
-});
+})
 
 // Name export actions outside of the module
 export const { bugAdded, bugAssignedToUser, bugResolved, bugRemoved } =
-  slice.actions;
+  slice.actions
 // Reducer has to be a default export in ducks pattern
-export default slice.reducer;
+export default slice.reducer
 
 // Selector
 
@@ -47,4 +47,14 @@ export const getUnresolvedBugs = createSelector(
   state => state.entities.bugs,
   state => state.entities.projects,
   (bugs, projects) => bugs.filter(bug => !bug.resolved)
-);
+)
+
+// Instead of setting getBugsByUser to a constant,
+// Set getBugsByUser to a different function
+// This function takes a parameter called userId
+// and returns the value that is returned from the createSelector function
+export const getBugsByUser = userId =>
+  createSelector(
+    state => state.entities.bugs,
+    bugs => bugs.filter(bug => bug.userId === userId)
+  )

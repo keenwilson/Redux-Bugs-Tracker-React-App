3e0485bd7c4d5fd67c007c28f219f8da508fb48e
Keen <keencwilson@gmail.com>
2022-02-01 21:24:10 -0800

    Adding bug and assigning a bug to a user

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -13,7 +13,7 @@ const slice = createSlice({
       bugs.list.push(action.payload)
     },
     bugAssignedToUser: (bugs, action) => {
-      const { bugId, userId } = action.payload
+      const { id: bugId, userId } = action.payload
       const index = bugs.list.findIndex(bug => bug.id === bugId)
       bugs.list[index].userId = userId
     },
@@ -77,6 +77,22 @@ export const addBug = bug =>
     method: 'post',
     data: bug,
     onSuccess: bugAdded.type
+  })
+
+export const resolveBug = id =>
+  apiCallBegan({
+    url: url + '/' + id,
+    method: 'patch',
+    data: { resolved: true },
+    onSuccess: bugResolved.type
+  })
+
+export const assignBugToUser = (bugId, userId) =>
+  apiCallBegan({
+    url: url + '/' + bugId,
+    method: 'patch',
+    data: { userId },
+    onSuccess: bugAssignedToUser.type
   })
 
 // Selector
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import configureStore from './configureStore'
-import { addBug, loadBugs } from './bugs'
+import { addBug, loadBugs, resolveBug, assignBugToUser } from './bugs'
 
 const store = configureStore()
-console.log(store.getState())
 
 // Actions that indicate API calls
 store.dispatch(loadBugs())
+
+setTimeout(() => store.dispatch(assignBugToUser(1, 4)), 2000)
+setTimeout(() => store.dispatch(resolveBug(1)), 2000)

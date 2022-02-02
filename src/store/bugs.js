8ba3ef45e0e0cd7ef6a00995f08b6fb3a4e96b04
Keen <keencwilson@gmail.com>
2022-02-01 16:36:13 -0800

    Use createAction and createReducer from Redux Toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,42 +1,29 @@
-// Action types
-const BUG_ADDED = "bugAdded"
-const BUG_REMOVED = "bugRemoved"
-const BUG_RESOLVED = "bugResolved"
+import { createAction, createReducer } from "@reduxjs/toolkit"
 
-// Action creators
-export const bugAdded = description => ({
-    type: BUG_ADDED,
-    payload: {
-        description
-    }
-})
+export const bugAdded = createAction("bugAdded")
+export const bugRemoved = createAction("bugRemoved")
+export const bugResolved = createAction("bugResolved")
+export const bugUpdated = createAction("bugUpdated")
 
-export const bugResolved = id => ({
-    type: BUG_ADDED,
-    payload: {
-        id
-    }
-})
-
-// Reducers
+// Reducer
 let lasId = 0
 
-// reducer has to be a default export in ducks pattern
-export default function reducer(state = [], action) {
-    switch (action.type) {
-        case BUG_ADDED:
-            return [
-                ...state,
-                {id: ++lasId,
-                description: action.payload.description,
-                resolved: false}
-            ]
-        case BUG_REMOVED:
-            return state.filter(bug => bug.id !== action.payload.id)
-        case BUG_RESOLVED:
-            // Update an existing bug object
-            return state.map(bug => bug.id !== action.payload.id ? bug : {...bug, resolved: true})
-        default:
-            return state
+// Reducer has to be a default export in ducks pattern
+export default createReducer([],{
+    // actions:  functions (event => event handler)
+    // Redux tollkit automatically pass this function to Immer for implementing immutable update pattern
+    [bugAdded.type]: (bugs, action) => {
+        bugs.push({id: ++lasId,
+            description: action.payload.description,
+            resolved: false})
+    },
+    [bugResolved.type]: (bugs, action) => {
+        const index = bugs.findIndex(bug => bug.id === action.payload.id)
+        bugs[index].resolved = true
+    },
+    [bugRemoved.type]: (bugs, action) => {
+        bugs.filter(bug => bug.id !== action.payload.id)
     }
-}
+
+})
+
